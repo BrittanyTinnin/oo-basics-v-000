f674846e1e51f53e3adf90d6e5814d73a2f56e7b
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-16 15:06:59 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,9 +4,9 @@ class Shoe
   attr_accessor :color, :size, :material, :condition, :brand
   # attr_reader :shoe
 
-  def initialize(shoe)
-    @shoe = shoe
-  end
+  # def initialize(shoe)
+  #   @shoe = shoe
+  # end
 
 
 
